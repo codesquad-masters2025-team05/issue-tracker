6a810c141059f5091ce0953c89b193e19c2f8078
2jiyong <jdragon2000@naver.com>
2025-05-19 16:37:18 +0900

    feat: IssueQueryServiceTest 통과하도록 구현

--- backend/src/main/java/com/team5/issue_tracker/label/query/LabelQueryRepository.java
+++ backend/src/main/java/com/team5/issue_tracker/label/query/LabelQueryRepository.java
@@ -3,8 +3,10 @@ package com.team5.issue_tracker.label.query;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Repository;
 import com.team5.issue_tracker.label.dto.LabelResponse;
 
+@Repository
 public class LabelQueryRepository {
   public Map<Long,List<LabelResponse>> getLabelListByIssueIds(List<Long> issueIds) {
     return Map.of();
--- backend/src/main/java/com/team5/issue_tracker/milestone/query/MilestoneQueryRepository.java
+++ backend/src/main/java/com/team5/issue_tracker/milestone/query/MilestoneQueryRepository.java
@@ -3,8 +3,10 @@ package com.team5.issue_tracker.milestone.query;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Repository;
 import com.team5.issue_tracker.milestone.dto.MilestoneResponse;
 
+@Repository
 public class MilestoneQueryRepository {
   public Map<Long, MilestoneResponse> getMilestonesByIds(List<Long> issueIds) {
     return Map.of();
