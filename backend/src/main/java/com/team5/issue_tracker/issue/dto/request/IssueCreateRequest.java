21f9f2645fdb51b26a6ebc3f64db017d59fcf4d4
dorkem <20233144@gs.cwnu.ac.kr>
2025-05-20 17:21:12 +0900

    feat: 이슈 생성 시 라벨과 담당자 저장 로직 메서드로 분리
    - DTO에서 @NotNull로 기본 유효성 검증 처리
    - 서비스 로직에서는 각 관계를 저장하는 메서드 분리

--- backend/src/main/java/com/team5/issue_tracker/issue/dto/request/IssueCreateRequest.java
+++ backend/src/main/java/com/team5/issue_tracker/issue/dto/request/IssueCreateRequest.java
@@ -3,6 +3,7 @@ package com.team5.issue_tracker.issue.dto.request;
 import java.util.List;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Getter;
@@ -19,7 +20,11 @@ public class IssueCreateRequest {
 
   @NotBlank(message = "본문을 작성해주세요.")
   private String body;
-  private Long assigneeId;
+
+  @NotNull(message = "담당자를 넣어주세요.")
+  private List<Long> assigneeIds;
+
+  @NotNull(message = "라벨을 넣어주세요.")
   private List<Long> labelIds;
   private Long milestoneId;
 }
