7765fd5e7039c85ebb22ab4fc758b15fe381af61
dorkem <20233144@gs.cwnu.ac.kr>
2025-05-20 11:11:52 +0900

    refactor: 로그인한 유저가 실제 DB에 존재하는지 확인하는 로직 추가
    - UserService는 IssueService에서만 참조하므로 순환참조 발생이 일어나지 않을 것 같습니다!

--- backend/src/main/java/com/team5/issue_tracker/issue/service/IssueService.java
+++ backend/src/main/java/com/team5/issue_tracker/issue/service/IssueService.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.team5.issue_tracker.issue.domain.Issue;
 import com.team5.issue_tracker.issue.dto.request.IssueCreateRequest;
 import com.team5.issue_tracker.issue.repository.IssueRepository;
+import com.team5.issue_tracker.user.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,10 +14,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class IssueService {
   private final IssueRepository issueRepository;
+  private final UserService userService;
 
   @Transactional
   public Long createIssue(IssueCreateRequest request) {
     Long userId = 1L; // TODO: 유저가 없으니 우선 임시데이터 넣음!
+    if (!userService.existsById(userId)) {
+      throw new IllegalArgumentException("존재하지 않는 사용자입니다.");
+    } //TODO: 커스텀 에러 만들지 고민중
     Issue issue = new Issue(
         request.getTitle(),
         request.getBody(),
