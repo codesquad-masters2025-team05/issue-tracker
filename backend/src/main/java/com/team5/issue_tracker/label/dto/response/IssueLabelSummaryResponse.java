926554f96b5034ced95d9a5fb62d2fe36d509e4b
dorkem <20233144@gs.cwnu.ac.kr>
2025-05-21 12:50:29 +0900

    feat: [CS-117] 필터링 용으로 사용되는 라벨 조회 쿼리 생성

--- backend/src/main/java/com/team5/issue_tracker/label/dto/response/IssueLabelSummaryResponse.java
+++ backend/src/main/java/com/team5/issue_tracker/label/dto/response/IssueLabelSummaryResponse.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class LabelSummaryResponse {
+public class IssueLabelSummaryResponse {
   private Long id;
   private String name;
   private String textColor;
--- backend/src/main/java/com/team5/issue_tracker/label/query/LabelQueryService.java
+++ backend/src/main/java/com/team5/issue_tracker/label/query/LabelQueryService.java
@@ -3,8 +3,10 @@ package com.team5.issue_tracker.label.query;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-import com.team5.issue_tracker.label.dto.response.LabelSummaryResponse;
 import com.team5.issue_tracker.label.dto.response.LabelPageResponse;
+import com.team5.issue_tracker.label.dto.response.LabelSummaryResponse;
+import com.team5.issue_tracker.label.dto.response.IssueLabelSummaryResponse;
+import com.team5.issue_tracker.label.dto.response.IssueLabelPageResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,6 +14,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LabelQueryService {
   private final LabelQueryRepository labelQueryRepository;
+
+  public IssueLabelPageResponse getFilterLabels() {
+    List<IssueLabelSummaryResponse> labels = labelQueryRepository.findIssueLabels();
+    return new IssueLabelPageResponse((long) labels.size(), 0L, (long) labels.size(), labels);
+  }
 
   public LabelPageResponse getAllLabels() {
     List<LabelSummaryResponse> labels = labelQueryRepository.findAllLabels();
