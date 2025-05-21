667340fbe7dd1c41b53b12e6b257b49b088161b4
2jiyong <jdragon2000@naver.com>
2025-05-21 16:53:48 +0900

    fix: 빈 리스트 전달시 쿼리 실행 안하도록 수정

--- backend/src/main/java/com/team5/issue_tracker/label/query/LabelQueryRepository.java
+++ backend/src/main/java/com/team5/issue_tracker/label/query/LabelQueryRepository.java
@@ -74,6 +74,9 @@ public class LabelQueryRepository {
   }
 
   public List<Long> getLabelIdsByNames(Collection<String> issueNames) {
+    if (issueNames.isEmpty()) {
+      return List.of();
+    }
     String sql = "SELECT id FROM label WHERE name IN (:issueNames)";
     MapSqlParameterSource params = new MapSqlParameterSource("issueNames", issueNames);
     return jdbcTemplate.queryForList(sql, params, Long.class);
