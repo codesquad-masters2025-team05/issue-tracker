8075419bb0da775860e8d73ea11e64f8be841d6b
Nago730 <jqk1797@gmail.com>
2025-05-23 12:48:58 +0900

    feat: 로고 버튼에 /issues로 이동하는 기능 추가

--- frontend/src/widgets/header/Header.tsx
+++ frontend/src/widgets/header/Header.tsx
@@ -2,12 +2,15 @@ import Logo from '@/assets/logo_medium.svg?react';
 import { ThemeToggleButton } from '@/shared/theme/ThemeToggleButton';
 import { User } from 'lucide-react';
 import type { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 const Header: FC = () => {
 	return (
 		<div className='flex flex-wrap items-center justify-between h-[94px] gap-4'>
 			<div className='w-50 flex items-center text-[var(--neutral-text-strong)]'>
-				<Logo className='w-[199px] h-10' />
+				<Link to='/issues' aria-label='이슈 목록으로 이동'>
+					<Logo className='w-[199px] h-10 cursor-pointer transition hover:opacity-80' />
+				</Link>
 			</div>
 
 			<div className='flex items-center gap-4'>
