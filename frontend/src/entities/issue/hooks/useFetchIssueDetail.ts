3b0a6faebb6a3d37244da8fe625732b6f1378230
Nago730 <jqk1797@gmail.com>
2025-05-27 17:25:14 +0900

    refactor: 오탈자 수정

--- frontend/src/entities/issue/hooks/useFetchIssueDetail.ts
+++ frontend/src/entities/issue/hooks/useFetchIssueDetail.ts
@@ -1,9 +1,9 @@
 import { fetchIssueDetail } from '@/entities/issue/api/issueAPI';
-import type { IssueDetailResponse } from '@/entities/issue/model/issue.types';
+import type { IssueDetailData } from '@/entities/issue/model/issue.types';
 import { useQuery } from '@tanstack/react-query';
 
 export function useFetchIssueDetail(id: number) {
-	return useQuery<IssueDetailResponse, Error>({
+	return useQuery<IssueDetailData, Error>({
 		queryKey: ['issues', id],
 		queryFn: () => fetchIssueDetail(id),
 	});
