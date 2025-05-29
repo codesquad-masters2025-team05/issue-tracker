45ef5b216f9f3cf362823613469fa389064191e1
Nago730 <jqk1797@gmail.com>
2025-05-29 11:57:36 +0900

    fix: 타입오류 수정

--- frontend/src/entities/label/hooks/useFetchLabelList.ts
+++ frontend/src/entities/label/hooks/useFetchLabelList.ts
@@ -1,9 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchLabels } from '../api/labelApi';
-import type { LabelListApiResponseDto } from '../model/label.types';
+import type { LabelListData } from '../model/label.types';
 
 export function useFetchLabelList() {
-	return useQuery<LabelListApiResponseDto, Error>({
+	return useQuery<LabelListData, Error>({
 		queryKey: ['labels'],
 		queryFn: fetchLabels,
 	});
