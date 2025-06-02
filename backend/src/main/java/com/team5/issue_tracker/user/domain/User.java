66ca30a2b3191dd2f3dc05425ead9a03aedf2f2a
dorkem <20233144@gs.cwnu.ac.kr>
2025-06-02 18:04:44 +0900

    feat: github oauth 로그인 추가

--- backend/src/main/java/com/team5/issue_tracker/user/domain/User.java
+++ backend/src/main/java/com/team5/issue_tracker/user/domain/User.java
@@ -1,8 +1,11 @@
 package com.team5.issue_tracker.user.domain;
 
+import java.time.Instant;
+
 import lombok.Getter;
 
 import org.springframework.data.annotation.Id;
+import com.team5.issue_tracker.common.auth.GitHubUser;
 
 @Getter
 public class User {
@@ -15,10 +18,25 @@ public class User {
   private final String password;
   private final String imageUrl;
 
+  private final Instant createdAt;
+  private Instant updatedAt;
+
   public User(String username, String email, String password, String imageUrl) {
     this.username = username;
     this.email = email;
     this.password = password;
     this.imageUrl = imageUrl;
+    Instant now = Instant.now();
+    this.createdAt = now;
+    this.updatedAt = now;
+  }
+
+  public static User oauthSignup(GitHubUser githubUser) {
+    return new User(
+        githubUser.getLogin(),
+        githubUser.getEmail(),
+        "GITHUB_USER",
+        githubUser.getAvatar_url()
+    );
   }
 }
