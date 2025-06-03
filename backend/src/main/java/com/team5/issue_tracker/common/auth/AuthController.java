0a1ee7ec463c07e4784b3e5f8dd202ce56ee415d
2jiyong <jdragon2000@naver.com>
2025-06-03 18:54:31 +0900

    refactor: api 경로 변경

--- backend/src/main/java/com/team5/issue_tracker/common/auth/AuthController.java
+++ backend/src/main/java/com/team5/issue_tracker/common/auth/AuthController.java
@@ -12,7 +12,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
   private final AuthService authService;
